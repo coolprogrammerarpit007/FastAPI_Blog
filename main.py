0debc73c1da8c9aee8e02dd8dd1820ba985d4f02
Arpit <arpit.mishra@microprixs.in>
2026-01-17 14:44:32 +0530

    Build The Post creation API with create and response schema using pydantic

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate,PostResponse
+
+
 app = FastAPI()
 app.mount("/static",StaticFiles(directory="static"),name="static")
 templates = Jinja2Templates(directory="templates")
@@ -66,13 +69,13 @@ def post_page(request:Request,post_id:int):
 # ********************** API Routes ******************************
 
 # API TO GET ALL THE POSTS
-@app.get("/api/posts")
+@app.get("/api/posts",response_model=list[PostResponse])
 def get_posts():
     return posts
 
 
 # API TO GET SINGLE POST
-@app.get("/api/posts/{post_id}")
+@app.get("/api/posts/{post_id}",response_model=PostResponse)
 def get_post(post_id:int):
     for post in posts:
         if post.get("id") == post_id:
@@ -81,6 +84,22 @@ def get_post(post_id:int):
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Post Not Found!")
 
 
+# API To create POST
+
+@app.post("/api/posts",response_model=PostResponse,status_code=status.HTTP_201_CREATED)
+
+def create_post(post:PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id":new_id,
+        "author":post.author,
+        "title" : post.title,
+        "content":post.content,
+        "date_posted":"January 17, 2026",
+    }
+    
+    posts.append(new_post)
+    return new_post
 ## StarletteHTTPException Handler
 @app.exception_handler(StarletteHTTPException)
 def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
