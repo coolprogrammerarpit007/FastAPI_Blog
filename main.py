d0d7f26f941a78f5836f09caaef033100e9bc44c
Arpit <arpit.mishra@microprixs.in>
2026-01-17 11:17:19 +0530

    Jinja 2 Template Engine and make it dynamic

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
+app.mount("/static",StaticFiles(directory="static"),name="static")
+templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
@@ -40,7 +43,9 @@ def get_posts():
 
 # Return HTML Response / Title of post in h1 Heading
 
-@app.get("/posts-title",response_class=HTMLResponse,include_in_schema=False)
+# @app.get("/posts-title",response_class=HTMLResponse,include_in_schema=False)
+@app.get("/home",include_in_schema=False,name="home")
 
-def post_heading():
-    return f"<h1>{posts[0]['title']}</h1>"
+def home(request:Request):
+    # return f"<h1>{posts[0]['title']}</h1>"
+    return templates.TemplateResponse(request,"home.html",{"posts":posts,"title":"HOME"})
